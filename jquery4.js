2e4d3ff1addf17b5a94dda9533afe697b2318c55
makiko1996 <t_makiko_1996@yahoo.co.jp>
2022-02-14 17:41:51 +0900

    [add]jquery5

---
+++ jquery4.js
@@ -0,0 +1,11 @@
+$(function(){
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color': '#0000FF'});
+  });
+});
+
+$(function(){
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color': '#ff0000'});
+   });
+});
